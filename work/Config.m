ba3c3a9ea06b1e3d7718103632010e1ecd36358c
Niklas Knäpper <niklas.knaepper@rub.de>
2023-11-22 15:52:35 +0100

    updated paths

--- work/Config.m
+++ work/Config.m
@@ -1,14 +1,15 @@
 classdef Config < handle
     properties
-        pathTIMIT = '../audio/';
-        pathDB_root = '../databases/';
+        pathTIMIT = '../audio/LibriSpeech/dev-clean/';
+        pathNoise = '../audio/';
+        pathDB_root = '../databases/';   
         pathDB_train = ''; % needs to be set in constructor
         pathDB_test = ''; % needs to be set in constructor
     end
     methods
         function obj = Config()
-            obj.pathDB_train = [obj.pathDB_root '2023_11_2_14_13_35\'];
-            obj.pathDB_test = [obj.pathDB_root '2023_11_2_14_13_35\'];
+            obj.pathDB_train = [obj.pathDB_root '2023_11_15_13_38_48\'];
+            obj.pathDB_test = [obj.pathDB_root '2023_11_16_12_19_41\'];
         end
     end
 end
